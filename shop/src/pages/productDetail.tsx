abb90cdbbac6176a25dfca058ac141dfe516ba9b
joo-chang <jooc0311@gmail.com>
2025-03-25 08:05:02 +0900

    useEffect 사용

--- shop/src/pages/productDetail.tsx
+++ shop/src/pages/productDetail.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { products } from '../data/products';
 import { Typography, Box } from '@mui/material';
@@ -6,17 +6,29 @@ import { Typography, Box } from '@mui/material';
 export const ProductDetail = () => {
   const { id } = useParams();
   const product = products.find((p) => p.id === Number(id));
-
+  const [showSale, setShowSale] = useState(true);
   if (!product) {
     return <div>Product not found</div>;
   }
 
+  useEffect(() => {
+    const time = setTimeout(() => {
+      setShowSale(false);
+    }, 3000);
+
+    return () => {
+      clearTimeout(time);
+    };
+  }, []);
   return (
     <Box
       sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}
     >
       <Typography variant="h4" component="h1" gutterBottom>
         {product.title}
+      </Typography>
+      <Typography variant="h6" gutterBottom>
+        {showSale && <div> 3초 안에 구매하면 할인.</div>}
       </Typography>
       <img
         src={product.image}
