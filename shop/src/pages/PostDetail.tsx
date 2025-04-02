a408111c534ddce1c71223b66be9cb36c6665e6a
joo-chang <jooc0311@gmail.com>
2025-04-03 08:04:00 +0900

    탭 기능 추가

--- shop/src/pages/PostDetail.tsx
+++ shop/src/pages/PostDetail.tsx
@@ -1,18 +1,22 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
-import { Typography, Box, Input } from '@mui/material';
+import { Typography, Box, Input, Tabs, Tab } from '@mui/material';
 import { PostInfo } from '../data/posts';
+import { TabContext, TabList } from '@mui/lab';
 export const PostDetail = () => {
   const { id } = useParams();
   const [post, setPost] = useState<PostInfo>();
   useEffect(() => {
-    fetch('https://jsonplaceholder.typicode.com/posts/' + id)
-      .then((response) => response.json())
-      .then((data: PostInfo) => {
-        setPost(data);
-      })
-      .catch((error) => console.error('Error fetching posts:', error));
-  }, [id]);
+    if (!post) {
+      fetch('https://jsonplaceholder.typicode.com/posts/' + id)
+        .then((response) => response.json())
+        .then((data: PostInfo) => {
+          setPost(data);
+        })
+        .catch((error) => console.error('Error fetching posts:', error));
+    }
+  }, [id, post]);
+
 
   return post ? (
     <Box
@@ -25,8 +29,10 @@ export const PostDetail = () => {
       <Typography variant="body1" paragraph>
         {post.body}
       </Typography>
+
     </Box>
   ) : (
     <div>post not found</div>
   );
 };
+
--- shop/src/pages/productDetail.tsx
+++ shop/src/pages/productDetail.tsx
@@ -1,7 +1,9 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { products } from '../data/products';
-import { Typography, Box, Input } from '@mui/material';
+import { Typography, Box, Input, Tab } from '@mui/material';
+import { TabList } from '@mui/lab';
+import { TabContext } from '@mui/lab';
 export const ProductDetail = () => {
   const { id } = useParams();
   const product = products.find((p) => p.id === Number(id));
@@ -10,6 +12,11 @@ export const ProductDetail = () => {
   if (!product) {
     return <div>Product not found</div>;
   }
+  const [value, setValue] = useState('1');
+
+  const handleChange = (event: React.SyntheticEvent, newValue: string) => {
+    setValue(newValue);
+  };
 
   useEffect(() => {
     const time = setTimeout(() => {
@@ -49,6 +56,27 @@ export const ProductDetail = () => {
         }}
       />
       <Typography variant="h6">Price: {price}</Typography>
+
+      <TabContext value={value}>
+        <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+          <TabList onChange={handleChange} aria-label="lab API tabs example">
+            <Tab label="Item One" value="1" onClick={() => setValue('1')} />
+            <Tab label="Item Two" value="2" onClick={() => setValue('2')} />
+            <Tab label="Item Three" value="3" onClick={() => setValue('3')} />
+          </TabList>
+        </Box>
+      </TabContext>
+      <TabPanel value={value} />
     </Box>
   );
 };
+
+function TabPanel({ value }) {
+  if (value == '1') {
+    return <div>Item One</div>;
+  } else if (value == '2') {
+    return <div>Item Two</div>;
+  } else if (value == '3') {
+    return <div>Item Three</div>;
+  }
+}
